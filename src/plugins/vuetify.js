86155ac8b9073373df0b7a3e5a1e2c128c295b50
SenatorCheese <d.pettersen@gmail.com>
2019-03-25 20:04:30 +0100

    added theme colours

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 
-Vue.use(Vuetify, {
+Vue.use(Vuetify,{
   iconfont: 'md',
-})
+  theme: {
+    primary:'#4caf50',
+    secondary: '#000000',
+    tertiary:'#607d8b',
+  }
+});
