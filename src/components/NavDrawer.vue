ad681de1348ea60b5eeca1464da38acdb2fc7975
SenatorCheese <d.pettersen@gmail.com>
2019-03-29 21:08:34 +0100

    Gave some them colours to NavDrawer items

--- src/components/NavDrawer.vue
+++ src/components/NavDrawer.vue
@@ -1,58 +1,52 @@
 <template>
-<div>
-     <v-navigation-drawer
+  <div>
+    <v-navigation-drawer
         app
         permanent
         clipped
         mobile-break-point="991"
         width="200"
-        >
-          <v-toolbar flat>
-            <v-list>
-              <v-list-tile>
-                <v-list-tile-title class="title">
-                 Dashboard
-                </v-list-tile-title>
-              </v-list-tile>
-            </v-list>
-          </v-toolbar>
-          
-          <v-divider></v-divider>
-
-          <v-list dense class="pt-0">
-            <v-list-tile
-              v-for="item in items"
-              :key="item.title"
-              @click=""
-              >
-              <v-list-tile-action>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-tile-action>
-
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-navigation-drawer>
-</div>
+    >
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              <span class="primary--text">Dashboard</span>
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-divider class="primary lighten-1"/>
+      <v-list dense class="pt-0" v-for="item in items">
+        <v-list-tile class="primary--text" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon class="primary--text">{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider/>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
-export default{
-    name: "navdrawer",
-    
-    data(){
-    return{
-      items:[
-        {title: 'Home', icon: 'dashboard'},
-        {title: 'Vessels', icon: 'directions_boat'},
-        {title: 'About', icon: 'question_answer'}
-      ],
-      right: null
-    }
-  }
+    export default {
+        name: "navdrawer",
 
-};
+        data() {
+            return {
+                items: [
+                    {title: 'Home', icon: 'dashboard'},
+                    {title: 'Vessels', icon: 'directions_boat'},
+                    {title: 'About', icon: 'question_answer'}
+                ],
+                right: null
+            }
+        }
+
+    };
 
 </script>
