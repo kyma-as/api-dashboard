09fe61a8859b1b479f4aec2e4c14d4d1e992e983
SenatorCheese <d.pettersen@gmail.com>
2019-03-29 22:28:50 +0100

    Removed unnecesary links now that we have a navDrawer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,6 @@
   <div class="home">
     <NavDrawer />
     <v-content>
-      <v-list>
-        <v-list-tile>
-          <router-link to="/">
-            <span>Logout</span>
-          </router-link>
-        </v-list-tile>
-        <v-list-tile>
-          <router-link to="/apioutput">
-            <span>API-output</span>
-          </router-link>
-        </v-list-tile>
-      </v-list>
     </v-content>
   </div>
 </template>
