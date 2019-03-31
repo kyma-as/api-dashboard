3edbb69da495f1160fe5f673692a957e42b4c151
Jbruhaug <Jbruhaug@hotmail.com>
2019-03-31 16:08:03 +0200

    fixed text placemnt to get index 0 shwoing

--- src/views/TestSide.vue
+++ src/views/TestSide.vue
@@ -15,7 +15,7 @@
             :transform="'translate(0, 0)'">
              <rect height="20" v-bind:width="25 * value" v-bind:y="i*30+20" fill="green"
              ></rect>
-             <text v-bind:y="i*30 + 5" fill="White">{{value}}</text>
+             <text v-bind:y="i*30 + 35" fill="White">{{value}}</text>
             </g>
         </svg>
     </v-content>
