aa7437082fb667d16ec65bea9bb0980e3095d1a3
Jbruhaug <Jbruhaug@hotmail.com>
2019-04-03 13:56:24 +0200

    yakse lagt til

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,11 @@
       <v-btn @click="calculatePath" >Tegn</v-btn> 
     </v-content> 
     <v-content>                       
-  <svg width="600" height="400" style="background: white" :transform="'translate(20, 0)'" >
+  <svg width="700" height="500" style="background: white" :transform="'translate(20, 0)'">
     <g >
-      <path :d="line" stroke="black" fill="none" />
+      <path :d="line" stroke="blue" fill="none" />
+      <path :d="yaksen" stroke="black" fill="none" :transform="'translate(-520, 0)'"/>
+      <path :d="xaksen" stroke="green" fill="none" :transform="'translate(-100, 0)'"/>
     </g>
   </svg>
     </v-content>
@@ -27,7 +29,12 @@ import {getApiDataTest} from "../scripts/getData";
       data() {
       return {
       rows: [],
+      xaksedata: [-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3],
+      yaksedata: [100000,0],
       line: '',
+      yaksen: '',
+      xaksen: '',
+      
     };
   },
   mounted() {
@@ -36,19 +43,31 @@ import {getApiDataTest} from "../scripts/getData";
   },
      methods:{
     getScales() {
-      const x = d3.scaleTime().range([550, 20]);
+      const x = d3.scaleTime().range([550, 40]);
       const y = d3.scaleLinear().range([350, 20]);
       d3.axisLeft().scale(x);
       d3.axisBottom().scale(y);
       x.domain(d3.extent(this.rows, (d, i) => i));
       y.domain([0, d3.max(this.rows, d => d)]);
-      return { x, y };
+      return { x, y,};
     },
     calculatePath() {
       const scale = this.getScales();
+
       const path = d3.line()
         .x((d, i) => scale.x(i))
         .y(d => scale.y(d));
+
+      const yakse = d3.line()
+        .x((d, i) => scale.x(i))
+        .y(d => scale.y(d));
+
+      const xakse = d3.line()
+        .x((d, i) => scale.x(i))
+        .y((d) => scale.y(d));
+
+      this.xaksen = xakse(this.xaksedata)
+      this.yaksen = yakse(this.yaksedata)
       this.line = path(this.rows);
     },
      getApiDataTest: async function () {
@@ -59,4 +78,5 @@ import {getApiDataTest} from "../scripts/getData";
       },
   }
 };
+
 </script>
