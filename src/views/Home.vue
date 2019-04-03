319d3382dcc625843de745c5cde1c3dc6277557b
Jbruhaug <Jbruhaug@hotmail.com>
2019-04-03 14:11:55 +0200

    added axes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,11 @@
       <v-btn @click="calculatePath" >Tegn</v-btn> 
     </v-content> 
     <v-content>                       
-  <svg width="700" height="500" style="background: white" :transform="'translate(20, 0)'">
+  <svg width="600" height="400" style="background: white" :transform="'translate(20, 0)'">
     <g >
       <path :d="line" stroke="blue" fill="none" />
-      <path :d="yaksen" stroke="black" fill="none" :transform="'translate(-520, 0)'"/>
-      <path :d="xaksen" stroke="green" fill="none" :transform="'translate(-100, 0)'"/>
+      <path :d="aksen" stroke="black" fill="none" :transform="'translate(-520, 20)'"/>
+      <path :d="aksen" stroke="black" fill="none" :transform="'translate(375, -175) rotate (90)'"/>
     </g>
   </svg>
     </v-content>
@@ -29,8 +29,7 @@ import {getApiDataTest} from "../scripts/getData";
       data() {
       return {
       rows: [],
-      xaksedata: [-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3],
-      yaksedata: [100000,0],
+      aksedata: [100000,0],
       line: '',
       yaksen: '',
       xaksen: '',
@@ -58,16 +57,11 @@ import {getApiDataTest} from "../scripts/getData";
         .x((d, i) => scale.x(i))
         .y(d => scale.y(d));
 
-      const yakse = d3.line()
+      const akse = d3.line()
         .x((d, i) => scale.x(i))
         .y(d => scale.y(d));
 
-      const xakse = d3.line()
-        .x((d, i) => scale.x(i))
-        .y((d) => scale.y(d));
-
-      this.xaksen = xakse(this.xaksedata)
-      this.yaksen = yakse(this.yaksedata)
+      this.aksen = akse(this.aksedata)
       this.line = path(this.rows);
     },
      getApiDataTest: async function () {
