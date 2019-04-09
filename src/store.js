519609b3b73e936d4f100b2c81fd6b9464d845c4
Daniel pettersen <d.pettersen@gmail.com>
2019-04-09 21:33:06 +0200

    added apiKey to userInfo state

--- src/store.js
+++ src/store.js
@@ -4,7 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    userInfo:{
+      apiKey:"ZGVtb0BreW1hZGF0YS5jb206ZGVtb2JydWtlcg"
+    }
+  },
+  mutations: {
+
+  },
   actions: {}
 });
