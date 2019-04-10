d64a013c2b7df26b5195ef121786f6065a5e12f4
Daniel pettersen <d.pettersen@gmail.com>
2019-04-10 19:02:55 +0200

    Created a mounted function on vessel to create retrieve current vessel

--- src/views/Vessel.vue
+++ src/views/Vessel.vue
@@ -2,7 +2,7 @@
 <div class="vessel">
     <NavDrawer />
     <v-content>
-        <span>Welcome to your Vessel!</span> 
+        <span>Welcome to {{currentVessel.name}}</span>
     </v-content>
 </div>
 </template>
@@ -12,7 +12,18 @@ import NavDrawer from "@/components/NavDrawer"
 export default {
     name: "vessel",
     components: {
-        NavDrawer
+        NavDrawer,
+    },
+    data(){
+      return {
+          currentVessel:""
+      }
+    },
+    mounted(){
+        this.currentVessel = this.$store.state.currentVessel;
+    },
+    computed:{
+
     }
     
 }
