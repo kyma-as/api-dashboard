c2b5a7d53fa0defb40ae90e9dfd05232a41ef0fb
Daniel pettersen <d.pettersen@gmail.com>
2019-04-17 19:47:21 +0200

    fetchVessels is working

--- src/store/actions.js
+++ src/store/actions.js
@@ -9,65 +9,65 @@
 // maybe use context.dispatch to start another fetch with the new data?
 // store.dispatch('fetchVessels').then(() => { ... }
 export default {
+    fetchVessels: async ({state, commit}) => {
+        let url = `${state.url}vessels`;
+        let header = state.header;
 
-  async function fetchVessels({ state, commit }, params) {
-    let url = `${state.url}${params}`;
-    let header = state.header;
-    let res = await fetch(url,header);
-    let json = await res.json();
+        let res = await fetch(url,header);
+        let vessels = await res.json();
 
-    let vessels = [];
-    for (let vessel of json) {
-      let newVessel = {
-        id: vessel.id,
-        name: vessel.name,
-        logVariables: []
-      };
-      vessels.push(newVessel);
+        let vesselArray = [];
+        for (let vessel of vessels) {
+            let newVessel = {
+                id: vessel.id,
+                name: vessel.name
+            };
+            vesselArray.push(newVessel);
+        }
+        commit('ADD_VESSELS', vesselArray);
+        // TODO: add a dispatcher to call getLogVariables
+        commit('INCREMENT');
+    },
+
+    getLogVariables: async ({state, commit}, vesselId) => {
+        // TODO: implement header function
+        let header = state.header;
+        let url = `${state.url}'/logvariables/find?vesselId='${vesselId}`;
+
+
+        let res = await fetch(url, header);
+        let json = await res.json();
+
+        let logVariableArray = [];
+        for (let logVariable of json) {
+            let newLogVariable = {
+                id: logVariable.id,
+                name: logVariable.name,
+                limitMin: logVariable.validLimitMinimum,
+                limitMax: logVariable.validLimitMaximum
+            };
+            logVariableArray.push(newLogVariable);
+        }
+        commit('APPEND_LOG_VARIABLES', {vesselId, logVariableArray});
+        commit('INCREMENT');
     }
-    commit('ADD_VESSELS', vessels);
-    // TODO: add a dispatcher to call getLogVariables
-    commit('INCREMENT');
-  },
 
-  async function getLogVariables({ state, commit }, vesselId) {
-    // TODO: implement header function
-    let header = state.header;
-    let url = `${state.url}'/logvariables/find?vesselId='${vesselId}`;
-    let res = await fetch(url, header);
-    let json = await res.json();
+    // TODO: figure out where this function belongs
+    /*
+    async function getLogData( {state, commit }, logVariableId, fromDate,
+      granularity = "Hour", toDate = getCurrentDate()) {
+      let header = state.header;
+      let url = `${state.url}/logdata/find?logVariableId=${logVariableId}
+            &granularity=${granularity}&fromDate=${fromDate}&toDate=${toDate}`;
 
-    let logVariableArray = [];
-    for (let logVariable of logVariables) {
-      let newLogVariable = {
-        id: logVariable.id,
-        name: logVariable.name,
-        limitMin: logVariable.validLimitMinimum,
-        limitMax: logVariable.validLimitMaximum
-      };
-      logVariableArray.push(newLogVariable);
-    }
-    commit('APPEND_LOG_VARIABLES', {vesselId, logVariableArray});
-    commit('INCREMENT');
-  }
-
-  // TODO: figure out where this function belongs
-  /*
-  async function getLogData( {state, commit }, logVariableId, fromDate,
-    granularity = "Hour", toDate = getCurrentDate()) {
-    let header = state.header;
-    let url = `${state.url}/logdata/find?logVariableId=${logVariableId}
-          &granularity=${granularity}&fromDate=${fromDate}&toDate=${toDate}`;
-
-    let res = await fetch(url, header);
-    let jsonLogData = res.json();
-    let dataArray = [];
-    for (let key in jsonLogData.data) {
-      dataArray.push(jsonLogData.data[key]);
-    }
-    return dataArray;
-  },*/
-
+      let res = await fetch(url, header);
+      let jsonLogData = res.json();
+      let dataArray = [];
+      for (let key in jsonLogData.data) {
+        dataArray.push(jsonLogData.data[key]);
+      }
+      return dataArray;
+    },*/
 
 
 }
