30d729013e7f96196c1422f889a46b772bf0ab82
Daniel pettersen <d.pettersen@gmail.com>
2019-05-07 22:37:34 +0200

    Made linechart work with state data

--- src/components/Graphs/LineChart.js
+++ src/components/Graphs/LineChart.js
@@ -1,74 +1,71 @@
-<template>
-  <div>
-    <li v-for="i, x in speed.log.data">
-      {{ x }}
-      {{ i }}
-    </li>
-  </div>
-</template>
+import {Line} from "vue-chartjs";
+import {mapGetters} from 'vuex';
 
-<script>
-    import {Line} from "vue-chartjs";
-    import {mapGetters } from 'vuex';
-    export default {
-        extends: Line,
-        computed:{
-            ...mapGetters([
-                'getSpeed'
-            ]),
-            speed() {
-                return this.getSpeed(this.$route.params.vesselid);
-            }
-        },
-
-        async mounted() {
-            let gpsSpeed = this.speed.gps.data;
-            let logSpeed = this.speed.log.data;
-            let labels = this.speed.labels;
-            labels.length = this.gpsSpeed.length;
-
-            let arrayLength = gpsSpeed.length;
-            let Diffvar1;
-            let Diffvar2;
-            let diff = [];
-
-            for (let i = 0; i < arrayLength; i++) {
-                Diffvar1 = gpsSpeed[i];
-                Diffvar2 = logSpeed[i];
-                if (Diffvar1 > Diffvar2) {
-                    diff.push(Diffvar1 - Diffvar2);
-                }
-                if (Diffvar1 < Diffvar2) {
-                    diff.push(Diffvar2 - Diffvar1);
-                }
-
-            }
-            this.renderChart(
-                {
-                    labels: labels,
-                    datasets: [
-                        {
-                            label: "Speed gps",
-                            borderColor: "green",
-                            data: gpsSpeed,
-                            pointRadius: 0
-                        },
-                        {
-                            label: "Speed log",
-                            borderColor: "blue",
-                            data: logSpeed,
-                            pointRadius: 0
-                        },
-                        {
-                            label: "Diff",
-                            data: diff,
-                            pointBackgroundColor: "red",
-                            showLine: false
-                        }
-                    ]
-                },
-                {responsive: true, maintainAspectRatio: false}
-            );
+export default {
+    extends: Line,
+    computed: {
+        ...mapGetters([
+            'getSpeed'
+        ])
+    },
+    data(){
+        return {
+            speed:{}
         }
-    };
-</script>
+    },
+
+    mounted() {
+
+        this.speed = this.getSpeed(this.$route.params.vesselid)
+        console.log(this.speed);
+        let gpsSpeed = [];
+        let logSpeed = [];
+        let labels = [];
+        for(let key in this.speed.gps.data){
+            gpsSpeed.push(this.speed.gps.data[key])
+            labels.push(key);
+        }
+        for(let key in this.speed.log.data){
+            logSpeed.push(this.speed.log.data[key]);
+        }
+
+        let diff = [];
+        for (let i = 0; i < gpsSpeed.length; i++) {
+            let Diffvar1 = gpsSpeed[i];
+            let Diffvar2 = logSpeed[i];
+            if (Diffvar1 > Diffvar2) {
+                diff.push(Diffvar1 - Diffvar2);
+            }
+            if (Diffvar1 < Diffvar2) {
+                diff.push(Diffvar2 - Diffvar1);
+            }
+
+        }
+        this.renderChart(
+            {
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Speed gps",
+                        borderColor: "green",
+                        data: gpsSpeed,
+                        pointRadius: 0
+                    },
+                    {
+                        label: "Speed log",
+                        borderColor: "blue",
+                        data: logSpeed,
+                        pointRadius: 0
+                    },
+                    {
+                        label: "Diff",
+                        data: diff,
+                        pointBackgroundColor: "red",
+                        showLine: false
+                    }
+                ]
+            },
+            {responsive: true, maintainAspectRatio: false}
+        );
+    }
+};
