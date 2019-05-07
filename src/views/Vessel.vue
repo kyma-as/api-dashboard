d62989b215e27c4b1ebc2f8370a189d21ac2a4ea
h181180 <adrsolheim@gmail.com>
2019-05-07 12:28:14 +0200

    add getters

--- src/views/Vessel.vue
+++ src/views/Vessel.vue
@@ -4,9 +4,10 @@
     <v-content>
         <span>Welcome to ship {{vessel.name}}.
           {{ vessel.id }} == {{ this.$route.params.vesselid }}</span>
-          <ul id="example-1">
-            <li v-for="lvar in logvariables">
-              <p> {{ lvar.name }} ---> {{ lvar.id }} </p>
+            <ul id="example-2">
+            <li v-for="i, x in speed.log.data">
+              {{ x }}
+              {{ i }}
             </li>
           </ul>
     </v-content>
@@ -15,7 +16,7 @@
 
 <script>
 import NavDrawer from "@/components/NavDrawer"
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
     name: "vessel",
@@ -31,15 +32,21 @@ export default {
       ...mapState([
         'vessels'
       ]),
+      ...mapGetters([
+        'getSpeed'
+      ]),
       logvariables() {
         return this.vessel.logVariables;
+      },
+      speed() {
+        return this.getSpeed(this.$route.params.vesselid);
       }
     },
     mounted() {
       const vessel = this.vessels
       .find(x => x.id === this.$route.params.vesselid);
       this.vessel = vessel;
-    } /*,
+    }/*,
     watch: {
     '$route.params': {
         function (newValue) {
