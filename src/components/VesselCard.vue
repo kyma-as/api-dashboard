e40e56b5705f99ce452372fee1df9452a6119ac5
h181180 <adrsolheim@gmail.com>
2019-05-28 15:06:27 +0200

    remove comments

--- src/components/VesselCard.vue
+++ src/components/VesselCard.vue
@@ -23,15 +23,12 @@
 </template>
 
 <script>
-// <v-flex xs12 sm6>Speed: 1000 km/h</v-flex>
-// <v-flex xs12 sm6>Fuel: 1000 litre</v-flex>
-// <v-flex xs12 sm12>Vessel Status: <span class="green--text">Good</span></v-flex>
     export default {
         data() {
           return {
             images: {
               vesselIcon: require('../assets/vessel.png')
-          }
+          }us
       }
         },
          components: {
